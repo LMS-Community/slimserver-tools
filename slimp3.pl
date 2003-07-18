e2518bc6c3d5c6d6410bf235a02f413591ea786d
Dean Blackketter <dean@slimdevices.com>
2003-07-18 19:41:34 +0000

    Initial revision
    git-svn-id: file:///home/awy/mirror/slim/trunk/tools@2 62299810-d8cb-41fd-93b7-d32162e5a4a4

---
+++ slimp3.pl
@@ -0,0 +1,140 @@
+#!/usr/bin/perl -w
+
+# SLIMP3 Server Copyright (C) 2001 Sean Adams, Slim Devices Inc.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License,
+# version 2.
+
+#
+# Command line interface for the controlling the SLIMP3
+#
+
+use strict;
+use Getopt::Long;
+use LWP::Simple;
+
+
+sub main {
+	my $httpaddr = undef;
+	my $httpport = undef;
+	my $player = undef;
+	my $command = undef;
+	my $p1 = undef;
+	my $p2 = undef;
+	my $p3 = undef;
+	my $p4 = undef;
+
+	GetOptions(
+		'httpaddr=s'	=> \$httpaddr,
+		'httpport=s'	=> \$httpport,
+		'player=s'	=> \$player,
+		'command=s'	=> \$command,
+		'p1=s'		=> \$p1,
+		'p2=s'		=> \$p2,
+		'p3=s'		=> \$p3,
+		'p4=s'		=> \$p3,
+	);
+
+	
+	if ( (!defined($command)) || 
+	     (!defined($httpaddr)) || 
+	     (!defined($httpport)) ) {
+		showUsage();
+	}
+	else {
+		executeCommand($httpaddr, $httpport, [$player, $command, $p1, $p2, $p3, $p4]);
+	}
+
+} # end sub main
+
+	# Commands are extracted from the parameters p0, p1, p2, p3, & p4.
+	#   For example:
+	#       http://host/status.html?p0=stop
+	# Both examples above execute a stop command, and sends an html status response
+	#
+	# Command parameters are query parameters named p0, p1, p2, p3 and p4
+	# 	For example:
+	#		http://host/status.m3u?p0=playlist&p1=jump&p2=2 
+	# This example jumps to the second song in the playlist and sends a playlist as the response
+	#
+	# If there are multiple players, then they are specified by the player id
+	#   For example:
+	#		http://host/status.html?p0=mixer&p1=volume&p2=11&player=10.0.1.203:69
+	#
+sub executeCommand {
+	my ($httpaddr, $httpport, $player, $command, $p1, $p2, $p3) = @_;
+	my $urlstring;
+	my $content = undef;
+	
+	$urlstring = "http://$httpaddr:$httpport/status?p0=$command";
+
+	if ( defined($p1) ) {
+		$urlstring .= "&p1=" . $p1;
+	}
+
+	if ( defined($p2) ) {
+		$urlstring .= "&p2=" . $p2;
+	}
+
+	if ( defined($p3) ) {
+		$urlstring .= "&p3=" . $p3;
+	}
+
+	if ( defined($p4) ) {
+		$urlstring .= "&p4=" . $p4;
+	}
+
+	if ( defined($player) ) {
+		$urlstring .= "&player=" . $player;
+	}
+
+	$urlstring .= "\n";
+	unless (defined ($content = get($urlstring))) {
+    		die "could not get $urlstring\n";
+	}
+
+	print $urlstring, "\n";
+
+	
+} # end sub executeCommand
+
+
+sub showUsage {
+
+        print <<EOF;
+Usage: $0 --httpaddr <host|ip> --httpport <port> --command <command> 
+          [--p1 <arg>] [--p2 <arg>] [--p3 <arg>] [--p4 <arg>] [--player <playerid>]
+
+        --httpaddr  => The hostname or ip address of the SLIMP3 web server
+	--httpport  => The port on which the SLIMP3 web server is listening
+	--command   => Pick from the 1st column of the list below
+        --p1        => Pick from the 2st column of the list below
+        --p2        => Pick from the 3rd column of the list below
+        --p3        => Pick from the 4th column of the list below
+        --p4        => Pick from the 5th column of the list below
+        --player    => Currently the "ip:port" of your player
+
+	COMMAND		P1	P2		P3	P4
+	 play
+	 pause		(0|1|)
+	 stop
+	 sleep		(0..n)
+	 playlist	play    <song>
+	 playlist	load    <playlist>
+	 playlist	append  <playlist>
+	 playlist	clear
+	 playlist	move    <fromoffset>	<tooffset>
+	 playlist	delete  <songoffset>
+	 playlist	jump    <index>
+	 mixer		volume  (0 .. 100)|(-100 .. +100)
+	 mixer		balance (-100 .. 100)|(-200 .. +200)
+	 mixer		base    (0 .. 100)|(-100 .. +100)
+	 mixer		treble  (0 .. 100)|(-100 .. +100)
+	 status
+	 display	<line1> <line2>		(duration)
+EOF
+
+} # end sub showUsage
+
+main();
+
---
+++ update_strings.pl
@@ -0,0 +1,97 @@
+#! /usr/bin/perl -w
+
+# Update the strings.txt file from the SLIMP3 project.
+#
+# Reads the strings.txt file and checks all items for the 
+# presence of an entry defined in $language.
+# If such an entry is not present, it displays entries that are
+# defined in @display and asks the input for $language.
+#
+# An additional sort is done on all translation strings 
+# (e.g. DE, FR, EN --> DE, EN, FR) 
+#
+# L. Hollevoet
+#
+
+$language = "NL";                  # language to update
+@display  = ("EN", "DE", "FR");    # display these languages as 'hints'
+
+# Open input and output files
+open (INPUT, "<strings.txt") || die "Could not open strings.txt";
+open (OUTPUT,">out.txt") || die "Could not open output.txt";
+
+# Process the input file
+while ($line = <INPUT>){
+
+    # copy comments blindly, they begin with #
+    if ($line =~ /^\#/){ 
+	print OUTPUT $line;
+	next; 
+    }
+
+    # search for an item, this begins with a capital
+    if ($line =~ /^([A-Z].+)/){
+	$item = $1;
+    }
+
+    # grab the contents of an entry, format: tab, XX, tab, string with XX=language 
+    if ($line =~ /^\s([A-Z]{2})\s(.+)/){
+	# language identifier in $1
+	# translated string in $2
+
+	# Do a check on multiple declarations of a language string under one item
+	# In case it occurs, print a warning and keep the last occurrence
+	if (defined $entries{$1}){
+	    print "WARNING: more than one '$1' in $item\n";
+	}
+
+	$entries{$1} = $2;
+    }
+
+    # if an empty line is encountered, check if something has to be written to the
+    # output file. Make sure to also recognise empty lines containing multiple tabs
+    if ($line =~ /^\s+$/){
+	if (defined %entries){
+	    update_item();
+	    print_item();
+	    undef %entries;
+	} else {
+	    print OUTPUT "\n";
+	}
+    }
+}
+
+# Close files
+close(INPUT);
+close(OUTPUT);
+
+# This sub will display the defined languages in @display and ask for 
+# the translated input for language $language 
+# If the user gives an empty line, then skip the addition
+sub update_item {
+    if (not defined $entries{$language}){
+	# if we get here the $language entry was not defined in the item
+	print "Item: $item\n";
+	foreach $lang (@display){
+	    if (defined $entries{$lang}){
+		print "\t$lang\t$entries{$lang}\n";
+	    }
+	}
+	print "\t$language\t";
+	chomp ($input = <STDIN>);
+
+	if ($input ne ""){
+	    $entries{$language} = $input;
+	}
+
+    }
+}
+
+# This sub writes an item to the output file
+sub print_item {
+    print OUTPUT "$item\n";
+    foreach $entry (sort keys %entries){
+	print OUTPUT "\t$entry\t$entries{$entry}\n";
+    }
+    print OUTPUT "\n";
+}
