2142d62928a5a69e87b2dd9868be8bf8d90c9389
Dean Blackketter <dean@slimdevices.com>
2007-03-28 00:21:48 +0000

    Update branding and copyright information to Logitech
    git-svn-id: file:///home/awy/mirror/slim/trunk/tools@11682 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- socketwrapper/socketwrapper.cpp
+++ socketwrapper/socketwrapper.cpp
@@ -1,6 +1,6 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: 4; c-basic-offset: 4 -*- */
 //
-// SlimServer Copyright (C) 2003-2004 Vidur Apparao, Slim Devices Inc.
+// SlimServer Copyright (C) 2003-2004 Vidur Apparao, Logitech.
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License,
 // version 2.
