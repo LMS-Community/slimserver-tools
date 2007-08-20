ddd4cac8e139989b741c58310117b7820407d5b4
Ben Klaas <bklaas@logitech.com>
2007-08-20 16:53:59 +0000

    Support for xml output
    git-svn-id: file:///home/awy/mirror/slim/trunk/tools@12620 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- find_translations_todo.pl
+++ find_translations_todo.pl
@@ -15,6 +15,7 @@ use Getopt::Long;
 
 my @default_supported_langs = qw/ EN DE ES IT FR NL /;
 my $command_args    = command_args();
+my $xml_template    = get_xml_template();
 my $supported_langs = $command_args->{'langs'};
 my $dirs            = $command_args->{'dirs'};
 
@@ -23,6 +24,9 @@ my %supported_langs = map { $_ => '1' } @$supported_langs;
 my $strings_files = get_strings_files();
 
 my @strings_to_translate;
+
+print $xml_template->{'root_header'} if $command_args->{'format'} eq 'xml';
+
 for my $string_file (@$strings_files) {
 	my $slurp = 0;
 	my %got_it; my @strings;
@@ -54,21 +58,44 @@ for my $string_file (@$strings_files) {
 	}
 	close(STRINGS);
 	if ($output) {
-		print "FILE:\t" . $string_file . "\n" if $command_args->{'verbose'};
-		print $output;
+		if ($command_args->{'format'} eq 'xml') {
+			my $filepath_header = $xml_template->{'filepath_header'};
+			$filepath_header =~ s/__FILEPATH/$string_file/g;
+			print $filepath_header;
+			print $output;
+			print $xml_template->{'filepath_footer'};
+		} else {
+			print "FILE:\t" . $string_file . "\n" if $command_args->{'verbose'};
+			print $output;
+		}
 	}
 }
+
+print $xml_template->{'root_footer'} if $command_args->{'format'} eq 'xml';
 
 sub compare_hashes {
 	my ($seen, $string, $strings) = @_;
 	my $missing_translations = missing_translations($seen);
 	my $return = "";
 	if ($missing_translations) {
-		$return .= "$string\n";
-		for my $line (@$strings) {
-			$return.= "\t$line->{'lang'}\t$line->{'string'}\n";
+		if ($command_args->{'format'} eq 'xml') {
+			my $string_header = $xml_template->{'string_header'};
+			$string_header =~ s/__STRING/$string/;
+			$return .= $string_header;
+			for my $line (@$strings) {
+				my $string_data = $xml_template->{'string_data'};
+				$string_data =~ s/__LANG/$line->{'lang'}/;
+				$string_data =~ s/__TRANSLATION/$line->{'string'}/;
+				$return .= $string_data;
+			}
+			$return .= $xml_template->{'string_footer'};
+		} else {
+			$return .= "$string\n";
+			for my $line (@$strings) {
+				$return.= "\t$line->{'lang'}\t$line->{'string'}\n";
+			}
+			$return .= "\n";
 		}
-		$return .= "\n";
 	}
 	return $return;
 }
@@ -93,7 +120,8 @@ sub get_strings_files {
 
 sub command_args {
 	my %args;
-	my $usage = "usage: find_translations_todo.pl (--verbose) (--dirs '...') (--langs '...')
+	my $usage = "usage: find_translations_todo.pl (--verbose) (--format [xml|txt]) (--dirs '...') (--langs '...')
+	--format defaults to txt. xml is the other option.
 	--verbose prints file information on each line
 	argument to --dirs is a list of dirs to search 
 		(defaults to '.')
@@ -103,6 +131,7 @@ sub command_args {
 		'help'	=>	\$args{'help'},
 		'dirs=s'	=>	\$args{'dirstring'},
 		'langs=s'	=>	\$args{'langstring'},
+		'format=s'	=>	\$args{'format'},
 		'verbose'	=>	\$args{'verbose'},
 	);
 	if ($args{'help'}) {
@@ -116,6 +145,8 @@ sub command_args {
 	}
 	$args{'dirs'} = \@dirs;
 
+	$args{'format'} eq 'txt' unless $args{'format'} eq 'xml';
+
 	my @langs = @default_supported_langs;
 	if ($args{'langstring'}) {
 		@langs = split/[^A-Z]+/, $args{'langstring'};
@@ -123,4 +154,26 @@ sub command_args {
 	$args{'langs'} = \@langs;
 
 	return \%args;
+}
+
+sub get_xml_template {
+	my %return;
+	$return{'root_header'} = <<XMLHEAD;
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+	<Root>
+XMLHEAD
+	$return{'filepath_header'} = <<FILEPATH;
+		<File RelativePath="__FILEPATH">
+FILEPATH
+	$return{'filepath_footer'} = "\t\t</File>\n";
+
+	$return{'string_header'} = <<XMLSTRING;
+			<TranslationUnit Id="__STRING">
+XMLSTRING
+	$return{'string_footer'} = "\t\t\t</TranslationUnit>\n";
+	$return{'string_data'} = <<STRINGDATA;
+				<Target Language = "__LANG">__TRANSLATION</Target>
+STRINGDATA
+	$return{'root_footer'} = "\t</Root>\n";
+	return \%return;
 }
