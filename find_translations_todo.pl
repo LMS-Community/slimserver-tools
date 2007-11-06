4a9c9046847a2a7ac281992c1d1e9162b19b533c
Ben Klaas <bklaas@logitech.com>
2007-11-06 17:20:25 +0000

    Also write an EN file
    git-svn-id: file:///home/awy/mirror/slim/trunk/tools@14421 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- find_translations_todo.pl
+++ find_translations_todo.pl
@@ -74,7 +74,8 @@ for my $string_file (@$strings_files) {
 		my $dir = "stringsFiles";
 		mkdir $dir unless -d $dir;
 		for my $LANG (@$supported_langs) {
-			next if $LANG eq 'EN';
+			# SLT team wants EN as well
+			#next if $LANG eq 'EN';
 			next if $found{$LANG} == $missing{$LANG};
 			my $template = 'strings.' . $args->{'format'} . '.tmpl';
 			my $outfile  = $dir . "/strings." . $LANG . "." . $args->{'format'};
