3041e6765e35342034b82d8769b2643413f1f264
Michael Herger <michael@slimdevices.com>
2009-08-21 14:02:33 +0000

    modify slt2strings helper script to merge Windows installer files (use --iss parameter)
    Bug: 13527
    git-svn-id: file:///home/awy/mirror/slim/7.4/trunk/tools@28237 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- strings/slt2strings.pl
+++ strings/slt2strings.pl
@@ -7,11 +7,11 @@ use File::Find;
 use File::Spec::Functions qw/:ALL/;
 
 my @defaultLanguages = qw/ EN CS DA DE ES FI IT FR NL NO SV PL RU /;
-my @defaultStringfiles = qw/ strings.txt global_strings.txt /;
+my $args = getArgs();
+
+my @defaultStringfiles = $args->{iss} ? qw/ strings.iss / : qw/ strings.txt global_strings.txt /;
 my %strings;
 my $debug = 1;
-
-my $args = getArgs();
 
 # TODO: make input folder configurable
 my $dirname = rel2abs('.');
@@ -77,8 +77,13 @@ find sub {
 			my ($tmpFile, $tmpFolder) = getTmpFile($shortName, $File::Find::dir);
 
 			my $originalStrings = getStringsFile($stringsFile);
-
-			mergeCustomStrings(\$originalStrings);
+			
+			if ($args->{iss}) {
+				mergeCustomIssStrings(\$originalStrings);
+			}
+			else {
+				mergeCustomStrings(\$originalStrings);
+			}
 
 			open(STRINGS, ">:utf8", $tmpFile) or die "Couldn't open $tmpFile for writing: $!\n";
 			binmode STRINGS;
@@ -200,21 +205,79 @@ sub sortStrings {
 	return join("\n", @translatedStrings) . "\n";
 }
 
+
+# .iss files for InnoSetup have a slightly different structure
+sub mergeCustomIssStrings {
+	my ($originalStrings) = @_;
+
+	foreach my $key (sort keys %strings) {
+		foreach my $lang (sort keys %{$strings{$key}}) {
+			
+			my $id  = lc($lang) . '.' . $key;
+			my $val = $strings{$key}->{$lang};
+			
+			next unless $id && $val;
+			
+			if ($$originalStrings !~ s/^$id=.*?$/$id=$val/ism) {
+
+				$$originalStrings .= "\n$id=$val";
+			}
+
+		}
+	}
+	
+	my $lastId = '';
+	# clean up the strings file
+	$$originalStrings = join("\n", 
+		# separate blocks of identical IDs with an empty line
+		map {
+			my ($id) = $_ =~ /[a-z]{2}\.(.*?)=/i;
+			
+			if ($id ne $lastId) {
+				$_ = "\n" . $_;
+				$lastId = $id;
+			}
+			$_;
+		} 
+		
+		#strip out empty lines
+		grep /[a-z]{2}\..*?=/i, 
+		
+		# sort strings by ID, then language
+		sort {
+			my ($langA, $idA) = $a =~ /([a-z]{2})\.(.*?)=.*/i;
+			my ($langB, $idB) = $b =~ /([a-z]{2})\.(.*?)=.*/i;
+			
+			$langA ||= '';
+			$langB ||= '';
+			$idA ||= '';
+			$idB ||= '';
+			
+			"$idA.$langA" cmp "$idB.$langB";
+		} split /\n/, $$originalStrings
+	);
+	
+	$$originalStrings =~ s/^cs/cz/mig;
+}
+
 sub getArgs {
 	my %args;
-	my $usage = "usage: slt2strings.pl (--langs '...') (--quiet) --dir '...'
+	my $usage = "usage: slt2strings.pl (--langs '...') (--iss) (--quiet) --dir '...'
 	argument to --dir is the root folder of the directory tree we want to search for strings.txt files
 
 	argument to --langs is a list of languages to check for translation 
-		(defaults to @defaultLanguages)\n";
+		(defaults to @defaultLanguages)\n
+		
+	--iss will merge the files in the InnoSetup Strings file format";
 
 	my $quiet;
 
 	GetOptions(
-		'help'      => \$args{'help'},
-		'langs=s'   => \$args{'langstring'},
-		'quiet'     => \$quiet,
-		'dir=s'     => \$args{'dir'},
+		'help'    => \$args{'help'},
+		'langs=s' => \$args{'langstring'},
+		'quiet'   => \$quiet,
+		'dir=s'   => \$args{'dir'},
+		'iss'     => \$args{'iss'},
 	);
 
 	if ($args{'help'} || !$args{'dir'}) {
